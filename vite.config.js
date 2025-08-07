4e6d621f4783aa4b51166a035afecceb77a2136e
Leomuch <muklasandrey@gmail.com>
2025-08-07 19:10:04 +0800

    ya

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite'
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
-<<<<<<< HEAD
     plugins: [
         laravel({
             input: [
@@ -17,18 +16,3 @@ export default defineConfig({
       tailwindcss(),
     ],
 });
-=======
-   plugins: [
-       laravel({
-           input: ['resources/css/app.css', 'resources/js/app.js'],
-           refresh: true,
-       }),
-        tailwindcss(),
-   ],
-   build: {
-       outDir: 'public/build',
-       manifest: true,
-       emptyOutDir: true,
-   }
-});
->>>>>>> 8d4f7b3ef0378cefa33bdc40c6dd4c9486e180f9
